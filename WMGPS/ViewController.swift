f088920f5bd15a78c8739d593efdad83fed3272d
Daniel Warner <danywarner@gmail.com>
2016-03-29 17:23:42 -0500

    GPS WORKING

--- WMGPS/ViewController.swift
+++ WMGPS/ViewController.swift
@@ -7,19 +7,67 @@
 //
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
 
+    @IBOutlet weak var latitudLbl: UILabel!
+    @IBOutlet weak var longitudLbl: UILabel!
+    @IBOutlet weak var altitudLbl: UILabel!
+    @IBOutlet weak var precisionHLbl: UILabel!
+    @IBOutlet weak var precisionVLbl: UILabel!
+    @IBOutlet weak var distanciaLbl: UILabel!
+    
+    var locationManager: CLLocationManager = CLLocationManager()
+    var startLocation: CLLocation!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+        locationManager.distanceFilter = 10.0
+        startLocation = nil
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        let latestLocation: AnyObject = locations[locations.count - 1]
+        
+        latitudLbl.text = String(format: "%.4f",
+                               latestLocation.coordinate.latitude)
+        longitudLbl.text = String(format: "%.4f",
+                                latestLocation.coordinate.longitude)
+        precisionHLbl.text = String(format: "%.4f",
+                                         latestLocation.horizontalAccuracy)
+        altitudLbl.text = String(format: "%.4f",
+                               latestLocation.altitude)
+        precisionVLbl.text = String(format: "%.4f",
+                                       latestLocation.verticalAccuracy)
+        
+        
+        if startLocation == nil {
+            startLocation = latestLocation as! CLLocation
+        }
+        
+        let distanceBetween: CLLocationDistance =
+            latestLocation.distanceFromLocation(startLocation)
+        
+        distanciaLbl.text = String(format: "%.2f", distanceBetween)
     }
+    
+    func locationManager(manager: CLLocationManager,
+                         didFailWithError error: NSError) {
+        
+    }
+    
+    
 
+    @IBAction func resetDistance(sender: AnyObject) {
+        startLocation = nil
+    }
 
 }
 
